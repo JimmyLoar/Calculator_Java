83f869e5ad63e193871e5f07bb22565645d0a591
JimmyLoar <play1596357895@gmail.com>
2023-05-28 01:20:39 +0300

    Add convert Roman numerals to Arabic numerals

--- test.java
+++ test.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 
 class Main{
 	static String[] oparation_array = {"+", "-", "*", "/"};
-
+    static int[] intervals={0, 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    static String[] numerals={"", "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
 
 	public static void main(String [] args){
 		Scanner in = new Scanner(System.in);
@@ -16,17 +17,31 @@ class Main{
 
 	public static String calc(String input){
 		String[] input_segmants = divideIntoComponents(input);
-		var string_a = input_segmants[0].trim();
-		var string_b = input_segmants[2].trim();
+		String string_a = input_segmants[0].trim();
+		String string_b = input_segmants[2].trim();
+		System.out.printf(" str a = '%s'\n str b = '%s'\n", string_a, string_b);
 
-		int a = convertInt(string_a);
-		int b = convertInt(string_b);
+		int a = toArabic(string_a);
+		int b = toArabic(string_b);
+		var aIsRoman = a == 0;
+		var bIsRoman = b == 0;
+
+		if (aIsRoman){
+			a = Integer.valueOf(string_a);
+		}	
+		if (bIsRoman){
+			b = Integer.valueOf(string_b);
+		}
+
+		if (aIsRoman != bIsRoman){
+			return "different number systems are used simultaneously";
+		}
 
 		String oparation = input_segmants[1];
 		System.out.printf(" a = %s\n b = %s\n op = %s\n", a, b, oparation);
 
 		int output = count(a, b, oparation);
-		return convertString(output);
+		return String.valueOf(output);
 
 	};
 
@@ -35,7 +50,7 @@ class Main{
 		String[] array = {};
 		for (int i = 0; i < oparation_array.length; i++){
 			array = string.split(String.format("\\%s", oparation_array[i]));
-			// System.out.printf("%s = %s | (%d) | %s\n", string, array, array.length, oparation_array[i]);
+			System.out.printf("%s = %s | (%d) | %s\n", string, array, array.length, oparation_array[i]);
 			if (array.length == 2){
 				String[] r_array = new String[3];
 				r_array[0] = array[0];
@@ -58,18 +73,51 @@ class Main{
 		};	
 		return 0;
 	};
-	
-
-	static int convertInt(String input){
-		return  Integer.valueOf(input);
-	};
 
 
-	static String convertString(int input){
-		return  String.valueOf(input);
-	};
+// Taken from https://ru.stackoverflow.com/questions/1234189/Как-преобразовать-римские-цифры-в-арабские-на-java
+	static int findFloor(final int number, final int firstIndex, final int lastIndex) {
+        if(firstIndex==lastIndex)
+            return firstIndex;
+        if(intervals[firstIndex]==number)
+            return firstIndex;
+        if(intervals[lastIndex]==number)
+            return lastIndex;
+        final int median=(lastIndex+firstIndex)/2;
+        if(median==firstIndex)
+            return firstIndex;
+        if(number == intervals[median])
+            return median;
+        if(number > intervals[median])
+            return findFloor(number, median, lastIndex);
+        else
+            return findFloor(number, firstIndex, median);
 
+    };
+
+    static String toRoman(final int number) {
+        int floorIndex=findFloor(number, 0, intervals.length-1);
+        if(number==intervals[floorIndex])
+            return numerals[floorIndex];
+        return numerals[floorIndex]+toRoman(number-intervals[floorIndex]);
+    }
+    
+    static int toArabic(String roman) {
+        int result = 0;
+        for (int i = intervals.length-1; i >= 0; i-- ) {
+            while (roman.indexOf(numerals[i]) == 0 && numerals[i].length() > 0) {
+                result += intervals[i];
+                roman = roman.substring(numerals[i].length());
+            }
+        }
+        // if (result == 0){
+        // return Integer.valueOf(roman);
+        // }
+        // else{
+        // 	return result;
+        // }
+        return result;
+    }
 }
 
-/// cd /d E:\Java ТЗ
-// java test.java
+// cd /D E:\Docks\Godot Progects\Calculator_Java\
