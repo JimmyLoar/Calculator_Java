80c3485da3b9a198e186a863d76fe72cbdab6996
JimmyLoar <play1596357895@gmail.com>
2023-05-28 14:53:45 +0300

    final update

--- test.java
+++ test.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 import java.util.*;
 
+// cd /D E:\Docks\Godot Progects\Calculator_Java\
+
 class Main{
+	static boolean isDebug = false;
 	static String[] oparation_array = {"+", "-", "*", "/"};
     static int[] intervals={0, 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
     static String[] numerals={"", "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
@@ -9,59 +12,145 @@ class Main{
 	public static void main(String [] args){
 		Scanner in = new Scanner(System.in);
 		String input = new String();
-		System.out.print("input:");
-		input = in.nextLine();
-		System.out.printf("output: %s\n\n", calc(input));
+
+		while (true){
+			System.out.print("input: ");
+			input = in.nextLine();
+			if (input.equals("exit") || input.equals("stop") || input.equals("break")) {
+				System.out.println("exited...");
+				System.exit(0);
+			}
+			else {
+				System.out.printf("output: %s\n\n", calc(input));
+			}
+		}
 	};
 
 
 	public static String calc(String input){
 		String[] input_segmants = divideIntoComponents(input);
+
+		try{
+		input_segmants[0].trim();
+		input_segmants[2].trim();
+
+		} catch(NullPointerException err){
+			debugPrint("throws Exception //err trim");
+			debugPrint("[error 0]:");
+			System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+			System.exit(0);
+		};
+
+
 		String string_a = input_segmants[0].trim();
 		String string_b = input_segmants[2].trim();
-		System.out.printf(" str a = '%s'\n str b = '%s'\n", string_a, string_b);
+		String oparation = input_segmants[1];
+
+
+		if (string_a == "" || string_b == "") {
+			debugPrint("[error 0]:");
+			System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+			System.exit(0);
+		}
+
+		debugPrint(String.format("	str a = '%s'\n	str b = '%s'\n", string_a, string_b));
 
 		int a = toArabic(string_a);
 		int b = toArabic(string_b);
-		var aIsRoman = a == 0;
-		var bIsRoman = b == 0;
+		var aIsArabic = a == -1;
+		var bIsArabic = b == -1;
 
-		if (aIsRoman){
+		if (aIsArabic){
 			a = Integer.valueOf(string_a);
 		}	
-		if (bIsRoman){
+		if (bIsArabic){
 			b = Integer.valueOf(string_b);
 		}
 
-		if (aIsRoman != bIsRoman){
-			return "different number systems are used simultaneously";
+		debugPrint(String.format("	a = %s\n	b = %s\n	op = %s\n", a, b, oparation));
+
+		if (aIsArabic != bIsArabic){
+			debugPrint("[error 1]:");
+			System.out.println("throws Exception //т.к. используются одновременно разные системы счисления");
+			System.exit(0);
 		}
 
-		String oparation = input_segmants[1];
-		System.out.printf(" a = %s\n b = %s\n op = %s\n", a, b, oparation);
+		else if (!aIsArabic) {
+			if (oparation == "-" && a < b) {
+				debugPrint("[error 2]:");
+				return "throws Exception //т.к. в римской системе нет отрицательных чисел";
+			}
+			else if ((oparation == "/" || oparation == "*") && (a <= 0 || b <= 0)) {
+				debugPrint("[error 2]:");
+				return "throws Exception //т.к. в римской системе нет отрицательных чисел";
+			}
+		}
 
+		if (a <= 0 || a > 10) {
+			if (aIsArabic) {
+				debugPrint("[error 3]:");
+				return "throws Exception //т.к. операнда 'a' выходят за приделы диапозона (0, 10)";
+				
+			}
+			debugPrint("[error 3]:");
+			return "throws Exception //т.к. операнда 'a' выходят за приделы диапозона (I, X)";
+		}
+		else if (b <= 0 || b > 10) {
+			if (bIsArabic) {
+				debugPrint("[error 3]:");
+				return "throws Exception //т.к. операнда 'b' выходят за приделы диапозона (0, 10)";
+				
+			}
+			debugPrint("[error 3]:");
+			return "throws Exception //т.к. операнда 'b' выходят за приделы диапозона (I, X)";
+		}
+		
 		int output = count(a, b, oparation);
+		if (!aIsArabic) {
+			return toRoman(output);
+		}
 		return String.valueOf(output);
-
 	};
+
+
+	static void debugPrint(String text){
+		if (isDebug) {
+			System.out.println(text);
+		}
+	}
 
 
 	static String[] divideIntoComponents(String string){
 		String[] array = {};
+		String[] r_array = new String[3];
+
+		int amount_op = 0;
+
 		for (int i = 0; i < oparation_array.length; i++){
 			array = string.split(String.format("\\%s", oparation_array[i]));
-			System.out.printf("%s = %s | (%d) | %s\n", string, array, array.length, oparation_array[i]);
+			debugPrint(String.format("	'%s' | %s | (%d) | %s\n", string, array, array.length, oparation_array[i]));
 			if (array.length == 2){
-				String[] r_array = new String[3];
 				r_array[0] = array[0];
 				r_array[1] = String.valueOf(oparation_array[i]);
 				r_array[2] = array[1];
-				return r_array;				
+				amount_op ++;
+				continue;		
+
+			} else if (array.length > 2) {
+				return new String[3];
 			}
 
 		};
-		return null;
-	}
+		if (amount_op > 1) {
+			return new String[3];
+		}
+		else if (amount_op < 1) {
+			debugPrint("[error 4]:");
+			System.out.println("throws Exception //т.к. строка не является математической операцией");
+			System.exit(0);
+		}
+		return r_array;
+	};
 
 
 	static int count(int a, int b, String oparation){
@@ -103,21 +192,17 @@ class Main{
     }
     
     static int toArabic(String roman) {
-        int result = 0;
+        int result = -1;
         for (int i = intervals.length-1; i >= 0; i-- ) {
             while (roman.indexOf(numerals[i]) == 0 && numerals[i].length() > 0) {
                 result += intervals[i];
                 roman = roman.substring(numerals[i].length());
             }
         }
-        // if (result == 0){
-        // return Integer.valueOf(roman);
-        // }
-        // else{
-        // 	return result;
-        // }
+        if (result != -1){
+        	result ++;
+        }
         return result;
     }
 }
 
-// cd /D E:\Docks\Godot Progects\Calculator_Java\
